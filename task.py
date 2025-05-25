3123d05a5f5764f6fe4395cb8d671c5f23991558
Xogan <yarok2005@mail.ru>
2025-05-25 12:28:37 +0700

    Решение задачи

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
-num_of_cars = int(input())
+num_cars = int(input())
 sum_speed = 0
 
-for _ in range(num_of_cars):
+for _ in range(num_cars):
     init_speed = int(input())
     sum_speed += init_speed
 
-average_speed = sum_speed / num_of_cars 
+average_speed = sum_speed / num_cars 
 
 print(average_speed)
