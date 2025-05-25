68e2971a3ea0d8b3233da780a10a950274ef7434
Xogan <yarok2005@mail.ru>
2025-05-25 12:52:11 +0700

    Функция добавлена

--- task.py
+++ task.py
@@ -1,10 +1,16 @@
 num_cars = int(input())
 sum_speed = 0
-
+check = 0
+ 
 for _ in range(num_cars):
     init_speed = int(input())
     sum_speed += init_speed
+    if init_speed >= 60:
+        check = 1
 
 average_speed = sum_speed / num_cars 
+
+if check == 1:
+    print('Yes')
 
 print(average_speed)
